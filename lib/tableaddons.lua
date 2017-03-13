dc8031936d3cade5c7a9b2140a1e949a600310ae
Matthew Whitney <matthew.whitney@match.com>
2017-03-13 13:19:21 -0500

    Fixed formatting

--- lib/tableaddons.lua
+++ lib/tableaddons.lua
@@ -1,11 +1,11 @@
 function table.count(tbl)
-    local count = 0
+  local count = 0
 
-    -- use pairs to loop through ALL items in the table
-    for _,i in pairs(tbl) do
-        count = count + 1
-    end
-    return count
+  -- use pairs to loop through ALL items in the table
+  for _,i in pairs(tbl) do
+    count = count + 1
+  end
+  return count
 end
 
 -- Be EXTREMELY carful of how you use the below table.slice, tables
@@ -36,36 +36,36 @@ end
 -- }
 
 function table.slice(tbl, first, count)
-    -- empty table to add the slice items to
-    local sliced = {}
+  -- empty table to add the slice items to
+  local sliced = {}
 
-    -- because we can't trust that a #tbl would give us an accurate count
-    local t = {}
-    local itemCount = 0
-    for _, i in pairs(tbl) do
-        table.insert(t, i)
-        itemCount = itemCount + 1
-    end
+  -- because we can't trust that a #tbl would give us an accurate count
+  local t = {}
+  local itemCount = 0
+  for _, i in pairs(tbl) do
+    table.insert(t, i)
+    itemCount = itemCount + 1
+  end
 
-    if first < 0 then
-        -- if we have a negative number for first, we need to count
-        -- backwards from the end
-        first = itemCount + first + 1
-    elseif first == 0 then
-        -- zero isn't valid
-        first = 1
-    end
+  if first < 0 then
+    -- if we have a negative number for first, we need to count
+    -- backwards from the end
+    first = itemCount + first + 1
+  elseif first == 0 then
+    -- zero isn't valid
+    first = 1
+  end
 
-    if count == nil then
-        count = itemCount
-    end
+  if count == nil then
+    count = itemCount
+  end
 
-    local last = first + count - 1
+  local last = first + count - 1
 
-    -- loop through the items wanted and add them to the table we'll return
-    for i=first, math.min(last, itemCount) do
-        table.insert(sliced, t[i])
-    end
+  -- loop through the items wanted and add them to the table we'll return
+  for i=first, math.min(last, itemCount) do
+    table.insert(sliced, t[i])
+  end
 
-    return sliced
+  return sliced
 end
